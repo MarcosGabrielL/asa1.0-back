84a4b37f8d82f6cf8798d5a382dd3b2db1703759
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-02 20:08:54 -0300

    Register Service

--- src/main/java/com/softsaj/AsaSpring/security/AppController.java
+++ src/main/java/com/softsaj/AsaSpring/security/AppController.java
@@ -26,6 +26,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -73,12 +75,12 @@ public class AppController {
     }
 
 @PostMapping("/process_register")
-public String processRegister(@RequestBody User user) {
+public ResponseEntity<User> processRegister(@RequestBody User user) {
    // BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     //String encodedPassword = passwordEncoder.encode(user.getPassword());
     //user.setPassword(encodedPassword);
      
-    userRepo.save(user);
+    User newUser = userRepo.save(user);
     
     //Cria Cinefilo
     Cinefilo cinefilo = new Cinefilo();
@@ -89,7 +91,7 @@ public String processRegister(@RequestBody User user) {
     vs.addCinefilo(cinefilo);
     
      
-    return "registersuccess";
+    return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 }
 
     @GetMapping("/users")
