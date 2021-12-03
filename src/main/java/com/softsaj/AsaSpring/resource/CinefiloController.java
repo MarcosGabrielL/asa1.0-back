767d0179ebfd70fa9ab735c65ab859ac14cb7561
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-03 08:32:48 -0300

    Update controller

--- src/main/java/com/softsaj/AsaSpring/resource/CinefiloController.java
+++ src/main/java/com/softsaj/AsaSpring/resource/CinefiloController.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  *
  * @author Marcos
  */
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/cinefilos")
 public class CinefiloController {
