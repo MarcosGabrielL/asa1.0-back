f6628614b15afc2070c10b35fa1039533fdaa25d
Marcos Gabriel <52946597+MarcosGabrielL@users.noreply.github.com>
2022-02-02 21:04:49 -0300

    Update Cinefilo.java

--- src/main/java/com/softsaj/AsaSpring/models/Cinefilo.java
+++ src/main/java/com/softsaj/AsaSpring/models/Cinefilo.java
@@ -5,17 +5,8 @@
  */
 package com.softsaj.AsaSpring.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.io.Serializable;
 import java.util.Objects;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 /**
  *
